23785a1b53e67a66f47219fce9201b709097fe98
Eduardo Marinho <eduardo.marinho@kenzie.com.br>
2023-03-03 17:17:35 -0300

    done

---
+++ script.js
@@ -0,0 +1,38 @@
+let itens = [
+    produto = {
+        image: './assets/img/painting/clock.jpg',
+        name: 'Smartwatch',
+        price: 'R$100,00',
+        type: 'Painting'
+    },
+    produto = {
+        image: './assets/img/painting/gamepad.jpg',
+        name: 'Controle Playstation',
+        price: 'R$100,00',
+        type: 'Painting'
+    },
+    produto = {
+        image: './assets/img/painting/personagem.jpg',
+        name: 'Robot',
+        price: 'R$100,00',
+        type: 'Painting'
+    },
+    produto = {
+        image: './assets/img/actions/animewoman.jpg',
+        name: 'Anime Woman',
+        price: 'R$100,00',
+        type: 'Action Figures'
+    },
+    produto = {
+        image: './assets/img/actions/dragonballpersonagem.jpg',
+        name: 'Goku Super Saiyajin',
+        price: 'R$100,00',
+        type: 'Action Figures'
+    },
+    produto = {
+        image: './assets/img/actions/starwarspersonagem.jpg',
+        name: 'Sr Yoda',
+        price: 'R$100,00',
+        type: 'Action Figures'
+    }
+];
